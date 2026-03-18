21ecf3059d3c0a6120f684713b45e1afab32706e
alexlukhanin <alex.lukhanin@hotmail.com>
2026-03-18 14:44:03 +0200

    Add contentPadding to LazyColumn

--- app/src/main/java/solutions/sgbrightkit/firetvdemo/MainScreen.kt
+++ app/src/main/java/solutions/sgbrightkit/firetvdemo/MainScreen.kt
@@ -1,19 +1,24 @@
 package solutions.sgbrightkit.firetvdemo
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.tv.material3.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.tv.material3.ExperimentalTvMaterial3Api
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.tv.material3.Card
+import androidx.tv.material3.ExperimentalTvMaterial3Api
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalTvMaterial3Api::class)
 @Composable
@@ -89,7 +94,11 @@ fun MovieCard(
             .height(135.dp)
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
-            Text(text = movie.title)
+            AsyncImage(
+                model = movie.cardImageUrl,
+                contentDescription = movie.title,
+                modifier = Modifier.fillMaxSize()
+            )
         }
     }
 }
